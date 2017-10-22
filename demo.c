b0d2a82555f88404b82e8ff26e09e7964cc8eca0
Caner Korkmaz <info@canerkorkmaz.com>
2017-10-22 22:14:32 +0300

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,31 +1,31 @@
- /* This code is written for reading physical memory address on linux distributions */ 
+/* This code is written for reading physical memory address on linux distributions */ 
 
- #include<stdio.h>
- #include<fcntl.h>
- #include<sys/mman.h>
- #include<unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
- int main()
- {
+int main()
+{
+    size_t STRUCT_PAGE_SIZE = getpagesize();
 
-   size_t STRUCT_PAGE_SIZE = getpagesize();
+    int num = 1;
+    int fd;
+    int *addr;
 
-   int num = 1;
-   int fd;
-   int *addr;
+    if ((fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0 )
+    {
+        printf("Error while opening the file. \n");
+        close(fd);
+        return 1;
+    }
 
-   if ((fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0 )
-   {
-     printf("Error while opening the file. \n");
-     close(fd);
-     return (-1);
-   }
+    addr = (int *)mmap(0, num*STRUCT_PAGE_SIZE, PROT_READ, MAP_PRIVATE, fd, 0x0000620000000000);
 
- addr = (int *)mmap(0, num*STRUCT_PAGE_SIZE, PROT_READ, MAP_PRIVATE, fd, 0x0000620000000000);
+    printf("addr: %p \n",addr);
+    printf("addr: %d \n",*addr);
 
- printf("addr: %p \n",addr);
- printf("addr: %d \n",*addr);
+    /* End of the program. */
 
-/* End of the program. */
-
+    return 0;
 }
